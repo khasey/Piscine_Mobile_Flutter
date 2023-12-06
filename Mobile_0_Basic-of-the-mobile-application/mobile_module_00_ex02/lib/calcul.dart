edc5443a3b2a7e1e8a4798e3f01c95aeae3927fc
khasey <92310470+khasey@users.noreply.github.com>
2023-12-06 12:55:20 +0100

    1.1

--- Mobile_0_Basic-of-the-mobile-application/mobile_module_00_ex02/lib/calcul.dart
+++ Mobile_0_Basic-of-the-mobile-application/mobile_module_00_ex02/lib/calcul.dart
@@ -8,16 +8,17 @@ class Calcul extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double appBarHeight = AppBar().preferredSize.height;
     double topPadding = MediaQuery.of(context).padding.top;
-    double displayContainerHeight = screenHeight * 0.4;
-    
+    double displayContainerHeight = screenHeight * 0.3;
+
     // Calcul de la hauteur disponible pour le GridView
-    double gridContainerHeight = screenHeight - appBarHeight - topPadding - displayContainerHeight;
+    double gridContainerHeight =
+        screenHeight - appBarHeight - topPadding - displayContainerHeight - 1;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('C A C U L A T O R',
             style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 4, 29, 49),
       ),
       body: Center(
         child: Column(
@@ -36,18 +37,18 @@ class Calcul extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: gridContainerHeight,
-              width: MediaQuery.of(context).size.width,
-              child: const Calculator(),
+            const Divider(
+              height: 1,
+              thickness: 1,
+              color: Colors.grey,
             ),
+            const Flexible(child: Calculator()),
           ],
         ),
       ),
     );
   }
 }
-
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -58,16 +59,32 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   final List<String> buttons = [
-    '7', '8', '9', 'C', 'AC',
-    '4', '5', '6', '+', '-',
-    '1', '2', '3', '*', '/',
-    '0', '.', '00', '=',
+    '7',
+    '8',
+    '9',
+    'C',
+    'AC',
+    '4',
+    '5',
+    '6',
+    '+',
+    '-',
+    '1',
+    '2',
+    '3',
+    '*',
+    '/',
+    '0',
+    '.',
+    '00',
+    '=',
   ];
 
   @override
   Widget build(BuildContext context) {
     // Détermination de l'orientation de l'appareil
-    bool isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    bool isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
 
     // Ajustement du nombre de colonnes en fonction de l'orientation
     int crossAxisCount = isPortrait ? 5 : 5;
@@ -76,22 +93,29 @@ class _CalculatorState extends State<Calculator> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     double gridHeight = isPortrait ? screenHeight * 0.6 : screenHeight * 0.8;
+    double aspect =
+        isPortrait ? screenWidth / gridHeight : screenWidth / gridHeight + 1;
+    print(aspect);
 
-    return SizedBox(
+    return Container(
+      color: Color.fromARGB(255, 4, 29, 49),
+      alignment: Alignment.bottomRight,
       height: gridHeight,
       width: screenWidth,
       child: GridView.count(
+        physics: const NeverScrollableScrollPhysics(),
         crossAxisCount: crossAxisCount,
         mainAxisSpacing: 5,
         crossAxisSpacing: 5,
-        childAspectRatio: 1, // Garantir que les boutons restent carrés
+        childAspectRatio: aspect, // Garantir que les boutons restent carrés
         children: buttons.map((String label) {
           return ElevatedButton(
             autofocus: true,
             style: ElevatedButton.styleFrom(
-              // Définition de la forme des boutons
+              // Définition de la forme des bou
+              elevation: 5,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(50),
                 side: const BorderSide(
                   color: Colors.grey,
                   width: 1,
@@ -100,11 +124,12 @@ class _CalculatorState extends State<Calculator> {
               ),
             ),
             onPressed: () {
-              print(label);
+              print("Button Pressed $label");
             },
             child: Text(
               label,
-              style: TextStyle(fontSize: isPortrait ? 15 : 12, color: Colors.black),
+              style: TextStyle(
+                  fontSize: isPortrait ? 15 : 12, color: Colors.black),
             ),
           );
         }).toList(),
